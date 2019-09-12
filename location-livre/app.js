5e9bba30b7f22ababd00336dbd24be797c363041
Thomas Marinier <marinierthomas974@gmail.com>
2019-09-12 16:45:15 +0400

    Test location velo v2

--- location-livre/app.js
+++ location-livre/app.js
@@ -4,11 +4,12 @@ var session = require('express-session')
 var mysql= require('mysql');
 
 //Template
-app.set('views engine','ejs');
+// app.set('views', __dirname + '/views');
+// app.set('views engine','ejs');
 
 
 // Middleware
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(session({
     secret: 'keyboard cat',
@@ -18,34 +19,79 @@ app.use(session({
   }))
 
 // Connetion
-// var connection = mysql.createConnection({
-//     host     : 'localhost',
-//     user     : 'root',
-//     password : '',
-//     database : 'location_livres'
-//   });
-//   connection.connect();
-//   console.log("Connexion ok")
-// module.exports=connection
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'location_livres'
+  });
+  connection.connect();
+  console.log("Connexion ok")
+module.exports=connection
 
 //Routes
 app.get('/', function(req, res) {
 
-   
-  
-    res.render('pages/index.ejs',{test:'Yooo'});
-   
+   // if (req.session.error){
+   //    res.locals.error=req.session.error
+   //    req.session.error=undefined
+   // }
+  res.sendFile(path.join(__dirname + 'pages/index.html'));
+   //res.render('pages/index.ejs');
+   //res.render('pages/index.html');
 });
 
 
-app.post('/', function(req, res) {
+// app.post('/', function(req, res) {
   
-   if (req.body.message===undefined||req.body.message===''){
+//    if (req.body.message===undefined||req.body.message===''){
+//       req.body.message="Il une erreur"   
+//       console.log(req.body)
+//       res.redirect('/')
       
-      console.log(req.body)
-      
-   }
+//    }
    
+// });
+
+
+app.post('/add', function(req, res) {
+	var titres = req.body.titres;
+	var date = req.body.date;
+	if (titres && date) {
+		connection.query('SELECT * FROM livres WHERE titre = ? AND date_parution = ?',[titre, date_parution], function(error, results, fields) {
+			if (results.length > 0) {
+				req.session.livres = true;
+				req.session.titres = titres;
+				res.redirect('/home');
+			} else {
+				res.send('Incorrect titres and/or date!');
+			}			
+			res.end();
+		});
+	} else {
+		res.send('Please enter titres and date!');
+		res.end();
+	}
 });
+
+app.get('/home', function(req, res) {
+	if (req.session.livres) {
+		res.send('Welcome back, ' + req.session.titres + '!');
+	} else {
+		res.send('Please login to view this page!');
+	}
+	res.end();
+});
+
+
+
+
+
+
+
+
+
+
 
 app.listen(8081);
+
